7fbc018ff4d7c115ea739695f6741015292d24a5
sasan <95457845+sasanaryan@users.noreply.github.com>
2023-10-09 18:43:14 +0330

    Update environment.ts

--- src/environment.ts
+++ src/environment.ts
@@ -1,6 +1,1 @@
-export const environment = {
-  production: false,
-  supabaseUrl: 'https://tmatdpokndlzvqsexhch.supabase.co',
-  supabaseKey:
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRtYXRkcG9rbmRsenZxc2V4aGNoIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY5NjY1MzcyNSwiZXhwIjoyMDEyMjI5NzI1fQ.q5wfsDeelXN7J8sijJIfiX2iJAh3B4GfNivN_6IrC_8',
-};
+
